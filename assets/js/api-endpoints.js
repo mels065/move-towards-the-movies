cb40a28fee46580e32b553771a2cd10e6674827c
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-06 21:18:40 -0400

    Add comments to zipcode to coordinates

--- assets/js/api-endpoints.js
+++ assets/js/api-endpoints.js
@@ -1,7 +1,11 @@
+// Mapbox API Key
 MAPBOX_API_KEY = "pk.eyJ1IjoibWVsbHVzYnJhbmRvbiIsImEiOiJja3FzcXQ0bG4ycWFrMm9tdmN3bmNxZHd3In0.zp0RMIIVSgMBMW_E-JycrQ";
+// The endpoint used for fetching data from geocoding
 MAPBOX_ENPOINT = "https://api.mapbox.com/geocoding/v5/mapbox.places";
+// Sets the API call to use postcodes for retrieving data from geocoding
 MAPBOX_PARAMS = `types=postcode&access_token=${MAPBOX_API_KEY}`;
 
+// Generates the full URL for the API call
 function generateApiCall(zipcode) {
     return `${MAPBOX_ENPOINT}/${zipcode}.json?${MAPBOX_PARAMS}`;
 }
