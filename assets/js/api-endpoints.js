271f38e42272aa1b6ec4a54591a3fa6560c76ef5
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-08 21:02:53 -0400

    Switch to Fandango API; create method for generating the signature needed for API calls (see docs)

--- assets/js/api-endpoints.js
+++ assets/js/api-endpoints.js
@@ -6,13 +6,23 @@ const MAPBOX_ENPOINT = "https://api.mapbox.com/geocoding/v5/mapbox.places";
 // Sets the API call to use postcodes for retrieving data from geocoding
 const MAPBOX_PARAMS = `types=postcode&access_token=${MAPBOX_API_KEY}`;
 
-/* MOVIEGLU API */
-// Movieglu API Key
-const MOVIEGLU_API_KEY = "y0r2AdkYtR8SmF0pDcO4u2qivf7rL0dN66BSQd7s";
-// The endpoint used for fetching nearby theaters
-const MOVIEGLU_ENDPOINT = "https://api-gate2.movieglu.com/cinemasNearby"
+/* FANDANGO API */
+const FANDANGO_API_KEY = "zkdcp444m83kn38rw355zrdw";
+const FANDANGO_SECRET = "7UmN5cmasY";
+const FANDANGO_ENDPOINT = "https://api.fandango.com/v1"
+const FANDANGO_PARAMS = `?&apikey=${FANDANGO_API_KEY}`
 
 // Generates the full URL for the API call
 function generateMapBoxApiCall(zipcode) {
     return `${MAPBOX_ENPOINT}/${zipcode}.json?${MAPBOX_PARAMS}`;
 }
+
+function generateFandangoSig() {
+    // Used this line of code
+    // https://developer.fandango.com/API__Sample_Code_nodejs
+    const seconds = Math.floor(new Date() / 1000);
+    const hash = CryptoJS.SHA256(`${FANDANGO_API_KEY}${FANDANGO_SECRET}${seconds}`);
+    return hash.toString(CryptoJS.enc.Hex);
+}
+
+console.log(generateFandangoSig());
---
+++ assets/js/get-theaters.js
@@ -0,0 +1,2 @@
+function getTheaters(coords) {
+}
