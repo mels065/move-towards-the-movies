7e9db9a6de23a1ddb8cfef34480cf6da82485f98
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-10 11:34:36 -0400

    Create function for getting search history data from local storage

--- assets/js/search-history.js
+++ assets/js/search-history.js
@@ -3,7 +3,12 @@ let searchHistory = new Set();
 function addSearchHistory(item) {
     if (!searchHistory.has(item)) {
         searchHistory.add(item);
-        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+        localStorage.setItem("searchHistory", JSON.stringify([...searchHistory]));
         // Update display of search history results
     }
+}
+
+function getSearchHistory() {
+    searchHistory = new Set(JSON.parse(localStorage.getItem("searchHistory")));
+    // Update display of search history results
 }
