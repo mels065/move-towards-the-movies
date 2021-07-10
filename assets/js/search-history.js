cacf54997bcdf8866b94b579ba7da6614ea73be2
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-10 11:49:09 -0400

    Create function for displaying results of past searches

--- assets/js/search-history.js
+++ assets/js/search-history.js
@@ -4,11 +4,25 @@ function addSearchHistory(item) {
     if (!searchHistory.has(item)) {
         searchHistory.add(item);
         localStorage.setItem("searchHistory", JSON.stringify([...searchHistory]));
-        // Update display of search history results
+        displaySearchHistory()
     }
 }
 
 function getSearchHistory() {
     searchHistory = new Set(JSON.parse(localStorage.getItem("searchHistory")));
-    // Update display of search history results
+    displaySearchHistory()
 }
+
+function displaySearchHistory() {
+    $("#searchList").html("");
+    if (searchHistory.size === 0) {
+        $("#searchList").append("<li>No search history at this time...</li>");
+    } else {
+        [...searchHistory].reverse().forEach((item) => {
+            const el = $(`<li class="search-history-item">${item}</li>`);
+            $("#searchList").append(el);
+        });
+    }
+}
+
+getSearchHistory();
