293f4008c240039da13e036d9524d66004ae1ccf
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-12 19:24:40 -0400

    Display showtimes for a movie in the movie modal

--- assets/js/display-theaters.js
+++ assets/js/display-theaters.js
@@ -1,15 +1,33 @@
-let theatres = [];
+function getShowtimeListElement(movie) {
+    const showtimeListEl = $('<ul id="showTimeList">');
+    const showtimesData = {};
+    movie.showtimes.forEach((showtime) => {
+        const { theatre: { id } } = showtime;
+        if (!showtimesData.hasOwnProperty(id)) {
+            showtimesData[id] = {
+                theatre: showtime.theatre.name,
+                times: []
+            }
+        }
+        showtimesData[id].times.push(moment(showtime.dateTime).format("HH:MM"));
+    });
 
-function getTheatres(movies) {
-    theatres = [];
-    movies.forEach((movie) => {
-        movie.showtimes.forEach(({ theatre }) => {
-            if (!theatreIdExists(theatre.id)) theatres.push(theatre)
-        })
-    })
+    for (let showtimes of Object.values(showtimesData)) {
+        const showtimeItemEl = $(
+            `<li class="showtimes-item">
+                ${showtimes.theatre} ${getTimesHtmlString(showtimes.times)}
+            </li>`
+        );
+        showtimeListEl.append(showtimeItemEl)
+    }
+
+    return showtimeListEl;
 }
 
-// Helper function to check if a theater already exists in the `theatres` array
-function theatreIdExists(id) {
-    return theatres.some((theatre) => theatre.id === id)
+function getTimesHtmlString(times) {
+    let html = "";
+    for (let i = 0; i < times.length; i++) {
+        html += `<span class="showtime-time">${times[i]}</span> `;
+    }
+    return html;
 }
